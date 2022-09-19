da1d1290b71dd2b8ed65a2ab59a7fb30e5fd64fe
zerozoo <zerozoo385@gmail.com>
2022-09-19 22:51:22 +0900

    feat(readme): readme와 PAT에 대한 에러 핸들링 추가

--- README.md
+++ README.md
@@ -1,12 +1,27 @@
-## 사전 설치
+# 해당 프로젝트를 실행하는 방법
 
+사전 준비물
+---
+- github auth token
+- node v12 이상
+- yarn 혹은 npm 혹은 pnpm
+
+
+> 해당 프로젝트는 github auth token을 필요로합니다.
+github personal access token을 생성하는 방법
+[Creating a personal access token](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token)
+
+
+설치 방법
+---
 ```bash
-yarn install
-npm install
+# 원하는 디렉토리에서 아래의 내용을 터미널에 복사 붙여넣기
+git clone https://github.com/zerozoo-a/submission_zerozoo.git
+$ yarn install # npm install
 ```
 
-## 실행 가능한 스크립트
 
+## 실행 가능한 스크립트
 ```bash
 yarn run start
 npm run start
--- src/fetchGraphQL.js
+++ src/fetchGraphQL.js
@@ -1,20 +1,29 @@
 // your-app-name/src/fetchGraphQL.js
 const REACT_APP_GITHUB_AUTH_TOKEN = process.env.REACT_APP_GITHUB_AUTH_TOKEN;
 async function fetchGraphQL(text, variables) {
-    const response = await fetch('https://api.github.com/graphql', {
-        method: 'POST',
-        headers: {
-            Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            query: text,
-            variables,
-        }),
-    });
+  if (
+    REACT_APP_GITHUB_AUTH_TOKEN == null ||
+    REACT_APP_GITHUB_AUTH_TOKEN === ""
+  ) {
+    throw new Error(
+      "THIS APP REQUIRES A GITHUB AUTHENTICATION TOKEN TO BE CONFIGURED. SEE README.md"
+    );
+  }
 
-    // Get the response as JSON
-    return await response.json();
+  const response = await fetch("https://api.github.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `bearer ${REACT_APP_GITHUB_AUTH_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: text,
+      variables,
+    }),
+  });
+
+  // Get the response as JSON
+  return await response.json();
 }
 
 export default fetchGraphQL;
